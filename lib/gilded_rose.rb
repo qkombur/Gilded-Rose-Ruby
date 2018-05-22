869ee6f4231f059e3f95db21206d1a6f81f55f68
Nicholas Auger <nick@qkombur.com>
2018-05-22 10:29:58 -0400

    specs passing and some basic refactoring

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,17 +7,57 @@ class GildedRose
     @quality = quality
   end
 
+  def agred_brie
+
+  end
+
+  def backstage_pass
+
+  end
+
+  def sulfuras
+
+  end
+
+  def sell_date
+
+  end
+
+  def subtract_day
+    @days_remaining = @days_remaining - 1
+  end
+
+  def mana_cake?
+    @name == "Conjured Mana Cake"
+  end
+
+  def not_aged_brie?
+    @name != "Aged Brie"
+  end
+
+  def not_backstage_pass?
+    @name != "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def ragnaros?
+    @name ==  "Sulfuras, Hand of Ragnaros"
+  end
+
+  def decrease_quality
+    return @quality -= 2 if mana_cake?
+    @quality -= 1
+  end
+
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
+    return if ragnaros?
+    if not_aged_brie? and not_backstage_pass?
       if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
+        decrease_quality
       end
     else
       if @quality < 50
         @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
+        if !not_backstage_pass?
           if @days_remaining < 11
             if @quality < 50
               @quality = @quality + 1
@@ -31,16 +71,15 @@ class GildedRose
         end
       end
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
+
+    # Why is this in the middle?
+    subtract_day
+
     if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
+      if not_aged_brie?
+        if not_backstage_pass?
           if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
+            decrease_quality
           end
         else
           @quality = @quality - @quality
