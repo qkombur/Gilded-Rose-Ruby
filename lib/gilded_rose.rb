01ebdefe8e33d10b56d2fd1ba6cd914d62fca6ee
Nicholas Auger <nick@qkombur.com>
2018-05-24 18:57:50 -0400

    Modifed readme and some minor refactoring, removed empty methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,21 +7,7 @@ class GildedRose
     @quality = quality
   end
 
-  def agred_brie
 
-  end
-
-  def backstage_pass
-
-  end
-
-  def sulfuras
-
-  end
-
-  def sell_date
-
-  end
 
   def subtract_day
     @days_remaining = @days_remaining - 1
@@ -31,13 +17,14 @@ class GildedRose
     @name == "Conjured Mana Cake"
   end
 
-  def not_aged_brie?
-    @name != "Aged Brie"
+  def aged_brie?
+    @name == "Aged Brie"
   end
 
-  def not_backstage_pass?
-    @name != "Backstage passes to a TAFKAL80ETC concert"
+  def backstage_pass?
+    @name == "Backstage passes to a TAFKAL80ETC concert"
   end
+
 
   def ragnaros?
     @name ==  "Sulfuras, Hand of Ragnaros"
@@ -48,25 +35,25 @@ class GildedRose
     @quality -= 1
   end
 
+  def increase_quality
+    return @quality += 1 if @quality < 50
+  end
+
   def tick
     return if ragnaros?
-    if not_aged_brie? and not_backstage_pass?
+    if !aged_brie? and !backstage_pass?
       if @quality > 0
         decrease_quality
       end
     else
       if @quality < 50
         @quality = @quality + 1
-        if !not_backstage_pass?
+        if backstage_pass?
           if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
+            increase_quality
           end
           if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
+            increase_quality
           end
         end
       end
@@ -76,8 +63,8 @@ class GildedRose
     subtract_day
 
     if @days_remaining < 0
-      if not_aged_brie?
-        if not_backstage_pass?
+      if !aged_brie?
+        if !backstage_pass?
           if @quality > 0
             decrease_quality
           end
@@ -85,9 +72,7 @@ class GildedRose
           @quality = @quality - @quality
         end
       else
-        if @quality < 50
-          @quality = @quality + 1
-        end
+        increase_quality
       end
     end
   end
